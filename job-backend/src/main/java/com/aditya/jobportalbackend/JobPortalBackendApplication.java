8e7230e6f9ffac4f6d7038bac5449e2a83ecb626
Aditya Tiwari <aditya.tiwari.cse.2022@tint.edu.in>
2026-03-24 22:54:20 +0530

    Added Backend (Spring Boot)

---
+++ job-backend/src/main/java/com/aditya/jobportalbackend/JobPortalBackendApplication.java
@@ -0,0 +1,14 @@
+package com.aditya.jobportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JobPortalBackendApplication.class, args);
+    }
+
+}
