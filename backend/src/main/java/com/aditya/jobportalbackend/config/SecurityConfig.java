93d5b759607067972db383ad00538470415474f6
Aditya Tiwari <aditya.tiwari.cse.2022@tint.edu.in>
2026-05-02 21:02:53 +0530

    fix cors imports + origin

--- backend/src/main/java/com/aditya/jobportalbackend/config/SecurityConfig.java
+++ backend/src/main/java/com/aditya/jobportalbackend/config/SecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -32,7 +32,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("*")); // allow all
+        configuration.setAllowedOriginPatterns(List.of(
+                "https://hirely-git-main-adityatiwari-devs-projects.vercel.app",
+                "https://hirely-six-navy.vercel.app"
+        )); // allow all
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
