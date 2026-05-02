b7105bb2ae41aaa10f011f53d21894fc2298c5b4
Aditya Tiwari <aditya.tiwari.cse.2022@tint.edu.in>
2026-05-02 20:52:45 +0530

    fix cors for all origins

--- backend/src/main/java/com/aditya/jobportalbackend/config/CorsConfig.java
+++ backend/src/main/java/com/aditya/jobportalbackend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://hirely-six-navy.vercel.app")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
