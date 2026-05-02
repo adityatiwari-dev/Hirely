1a53f38bbd8a425ee2797d346d6049f4e8d2cb86
Aditya Tiwari <aditya.tiwari.cse.2022@tint.edu.in>
2026-05-02 21:08:57 +0530

    fix cors credentials issue

--- backend/src/main/java/com/aditya/jobportalbackend/config/SecurityConfig.java
+++ backend/src/main/java/com/aditya/jobportalbackend/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         )); // allow all
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
